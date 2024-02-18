5bb699c845f8aee67dd8ed6e7abc8ff902010a97
liliul <liliuestudo@gmail.com>
2024-02-18 20:27:05 -0300

    feat: lendo o banco de dados firestore userGithub-firebase

--- userGithub/src/script.js
+++ userGithub/src/script.js
@@ -1,5 +1,26 @@
+// id
+const container = document.getElementById('container')
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 
-console.log(firebaseApp)
+// console.log(firebaseApp)
+
+db.collection('userGithub').get()
+	.then((snapshot) => {
+		const mostrarDocs = snapshot.docs.forEach(doc => {
+			const {name, createdAt} = doc.data()
+
+			const docsHtml = `
+				<section class="card">
+					<h1>${name}</h1>
+					<br>
+					<span class="data">${createdAt.toDate()}</span>
+				</section>
+			` 
+			container.innerHTML += docsHtml
+	})
+}).catch((err) => {
+		console.log(err.menssage)
+})
