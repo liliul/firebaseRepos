f82373b5bf119b008635f28031b50f33ba4ed9a1
liliul <liliuestudo@gmail.com>
2024-02-25 19:57:54 -0300

    fix: erro data e hora com onSnapshot userGithub-firebase

--- userGithub/src/script.js
+++ userGithub/src/script.js
@@ -121,21 +121,23 @@ content.addEventListener('click', (e) => {
 
 db.collection('userGithub').onSnapshot((doc) => {
 	content.innerHTML = ''
-	
+
 	doc.docs.map((doc) => {
 		console.log(doc.data())
-		const {name, createdAt} = doc.data()
-		const docsHtml = document.createElement('div')
-		docsHtml.innerHTML = `
-			<section class="card" data-card="${doc.id}">
-				<h1>${name}</h1>
-				<br>
-				<span class="data">${createdAt}</span>
-				<hr>
-				<button data-remove="${doc.id}">Deletar</button>
-				<button id="editarDocs" data-editar="${doc.id}">Editar</button>
-			</section>
-		` 
-		content.appendChild(docsHtml)
+		if (!doc.metadata.hasPendingWrites) {
+			const {name, createdAt} = doc.data()
+			const docsHtml = document.createElement('div')
+			docsHtml.innerHTML = `
+				<section class="card" data-card="${doc.id}">
+					<h1>${name}</h1>
+					<br>
+					<span class="data">${createdAt.toDate()}</span>
+					<hr>
+					<button data-remove="${doc.id}">Deletar</button>
+					<button id="editarDocs" data-editar="${doc.id}">Editar</button>
+				</section>
+			` 
+			content.appendChild(docsHtml)
+		}	
 	})
 })	
