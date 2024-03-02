bd9b7409e0d84827f648a4724c65bb673e04b1bf
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-02 18:52:45 -0300

    chore: limpando o index.html firebase-userGithub

--- userGithub/src/script.js
+++ userGithub/src/script.js
@@ -4,6 +4,17 @@ const content   = document.getElementById('content')
 const addUserGithub = document.querySelector('[data-js="add-user"]')
 const updateUserGithub = document.querySelector('[data-js="up-user"]')
 
+// https://firebase.google.com/docs/web/modular-upgrade?hl=pt#window-compat
+const firebaseConfig = {
+	apiKey: "AIzaSyAQg4btb7xBFNoaL4C-TWarZxMOQPxRdF0",
+	authDomain: "user-github.firebaseapp.com",
+	projectId: "user-github",
+	storageBucket: "user-github.appspot.com",
+	messagingSenderId: "921139920558",
+	appId: "1:921139920558:web:1309e906441151746ff73f",
+	measurementId: "G-L7146BC1WM"
+};
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
