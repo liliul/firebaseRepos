b268bfdd0b9b287b262880ac75b7f3420331381a
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-06 18:50:09 -0300

    fix: verificando input se esta enviando espaço vazio para firestore firebase-userGithub

--- userGithub/src/service/add.js
+++ userGithub/src/service/add.js
@@ -1,19 +1,22 @@
 addUserGithub.addEventListener('submit', async (e) => {
 	e.preventDefault()
-	if(e.target.user.value === '') return
+	if(e.target.user.value.trim()) {
+		console.log(e.target.user.value.trim())			
+		// await getApiGithub(e.target.user.value)
 
-	await getApiGithub(e.target.user.value)
+		db.collection('userGithub').add({
+			name: e.target.user.value,
+			createdAt: firebase.firestore.FieldValue.serverTimestamp()
+		})
+		.then(() => {
+			e.target.user.value = ''
+			console.log('add sucessFull')
+		}).catch(err => {
+			console.log(err.menssage)
+		})	
+	}
 
-	db.collection('userGithub').add({
-		name: e.target.user.value,
-		createdAt: firebase.firestore.FieldValue.serverTimestamp()
-	})
-	.then(() => {
-		e.target.user.value = ''
-		console.log('add sucessFull')
-	}).catch(err => {
-		console.log(err.menssage)
-	})
+	
 })
 
 // api github
