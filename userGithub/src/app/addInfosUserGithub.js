6bf5834ea82256110a78b83ba6d6cc01dff04afc
liliul <liliuestudo@gmail.com>
2024-03-11 18:51:46 -0300

    feat: adicionando infos do github no doc infos-github do firestore userGithub-firebase

---
+++ userGithub/src/app/addInfosUserGithub.js
@@ -0,0 +1,58 @@
+addUserGithub.addEventListener('submit', async (e) => {
+	e.preventDefault()
+	if(e.target.user.value.trim()) {
+		console.log(e.target.user.value.trim())			
+		
+		/**
+		 * @function // tras informação do usuario do github
+		 *
+		 *  
+		*/
+		const resApi = await getApiGithub(e.target.user.value)
+
+		/**
+		 * // if verifica se nome do usuario é valido se nao retorna alert
+		*/
+		if(resApi.message === 'Not Found') {
+			return alert('Nome do github invalido')
+		}
+
+		const result = {'id': resApi.id, 'name': resApi.name, 'login': resApi.login, 'avatar': resApi.avatar_url}
+		/**
+		 * // if verifica se id do usuario é undefined e so retorna
+		*/
+		if(result.id === undefined) return alert('Id usuario github undefined')
+		console.log(result)
+		/**
+		 * // if verifica se name do usuario é null e retorna alert
+		*/
+		if (result.name === null) return alert('Usuario github null')
+		/**
+		 * @description // adicionado items no firestore
+		*/
+		db.collection('infos-github').add({
+			id: result.id,
+			name: result.name,
+			login: result.login,
+			avatar: result.avatar,
+			createdAt: firebase.firestore.FieldValue.serverTimestamp()
+		})
+		.then(() => {
+			e.target.user.value = ''
+			console.log('add sucessFull')
+		}).catch(err => {
+			console.log(err.menssage)
+		})	
+	}
+
+	
+})
+
+// api github
+const getApiGithub = async (name) => {
+	const req = await fetch(`https://api.github.com/users/${name}`);
+	const res =  await req.json();
+
+	return res
+}	
+// getApiGithub('liliul')
