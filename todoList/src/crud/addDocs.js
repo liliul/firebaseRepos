ddcecfe0024ea167700b911fa07699054c62ec2a
liliul <liliuestudo@gmail.com>
2024-03-20 01:07:28 -0300

    feat: adicionando no firestore todoList-firebase

--- todoList/src/crud/addDocs.js
+++ todoList/src/crud/addDocs.js
@@ -1,3 +1,26 @@
+import { db } from '../config/firestore.js'
+import { collection, addDoc } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js'
+
+
 // id no html
 const idInputTask = document.getElementById('idInputTask');
 const buttonTask = document.getElementById('button-task');
+
+buttonTask.addEventListener('click', async () => {
+    const eventInput = idInputTask.value.trim()
+
+    if (eventInput) {
+        console.log(eventInput)
+
+        try {
+            const adicionarDocs = await addDoc(collection(db, 'todo-list'), {
+                input: eventInput,
+                taskCheck: 'checked'
+            })
+            console.log('id': adicionarDocs.id)
+        } catch (e) {
+            console.error('addDoc', e)
+        }
+
+    }
+})
--- todoList/src/index.js
+++ todoList/src/index.js
@@ -1,3 +1,4 @@
 // https://firebase.google.com/docs/web/alt-setup?hl=pt&authuser=0
 
 import './crud/getDocs.js'
+import './crud/addDocs.js'
