bc3838265babd84b21ef68f5ea5e16644aed10c9
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-25 17:52:11 -0300

    feat: limpando o input depois de criar tarefas todoList-firebase

--- todoList/src/crud/addDocs.js
+++ todoList/src/crud/addDocs.js
@@ -1,8 +1,6 @@
 import { db } from '../config/firestore.js'
 import { collection, addDoc } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js'
 
-
-// id no html
 const idInputTask = document.getElementById('idInputTask');
 const buttonTask = document.getElementById('button-task');
 
@@ -10,14 +8,15 @@ buttonTask.addEventListener('click', async () => {
     const eventInput = idInputTask.value.trim()
 
     if (eventInput) {
-        console.log(eventInput)
-
+        document.querySelector('#idInputTask').value = '';
+        
         try {
+
             const adicionarDocs = await addDoc(collection(db, 'todo-list'), {
                 input: eventInput,
                 taskCheck: ""
             })
-            console.log('id', adicionarDocs.id)
+
         } catch (e) {
             console.error('addDoc', e)
         }
