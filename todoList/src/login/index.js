19a15c798716cb1de33913b520df9bffa4c7372f
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-28 19:29:18 -0300

    chore: rename dos arquivos html para iniciar com a tela de login todoList-firebase

--- todoList/src/login/index.js
+++ todoList/src/login/index.js
@@ -1,25 +1,28 @@
+import { db } from '../config/firestore.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
 
 let email = 'gokufirestore@email.com'
 let password = '123456'
 
+let email = 'narutofirestore@email.com'
+let password = '101919'
+
 // fazer login
-const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => { 
-    const user = userCredential.user;
-   	console.log('auth ', user.providerData[0])
-   	if (user.providerData[0].email === email && user.providerData[0].providerId === password) {
-   		console.log('sim')
-   	}else {
-   		console.log('nao')
-   	}
-})
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-   	console.log('e1', errorCode, 'e2', errorMessage)
-});
+// const auth = getAuth();
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => { 
+//     const user = userCredential.user;
+//    	console.log('auth ', user.uid)
+   	
+//     setTimeout(() => {
+//       window.location = './index2.html';
+//     }, 3000)
+// })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//    	console.log('e1', errorCode, 'e2', errorMessage)
+// });
 
 
 // criar uma conta
@@ -28,7 +31,7 @@ signInWithEmailAndPassword(auth, email, password)
 //   .then((userCredential) => {
 //     // Signed up 
 //     const user = userCredential.user;
-//     // ...
+//     console.log(user)
 //   })
 //   .catch((error) => {
 //     const errorCode = error.code;
