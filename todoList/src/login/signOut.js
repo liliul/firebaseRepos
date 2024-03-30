07c9cf4f508eaa1dd0553f9f2777eb50127626f0
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-30 20:05:16 -0300

    feat: signOut deslogando a conta do auth do firebase todoList-firebase

--- todoList/src/login/signOut.js
+++ todoList/src/login/signOut.js
@@ -1,17 +1,26 @@
 import { db } from '../config/firestore.js';
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
 
-const auth2 = getAuth();
+const auth = getAuth();
 
-onAuthStateChanged(auth2, (user) => {
+onAuthStateChanged(auth, (user) => {
   if (user) {
     document.querySelector('.formLogin').style.display = 'flex';
 
     document.querySelector('.s-email').textContent = user.email;
-  } else {
-    // Usuário não está logado
-    console.log("Nenhum usuário está logado.");
 
-    document.querySelector('.formLogin').style.display = 'none';
+    const deslogar = document.querySelector('.b-signOut');
+    deslogar.addEventListener('click', () => {
+    
+    	const auth = getAuth();
+		signOut(auth).then(() => {
+    		document.querySelector('.formLogin').style.display = 'none';
+    		window.location.href = './index.html';
+		}).catch((error) => {
+			console.log('Erro ao deslogar', error)
+		});
+    })
+  } else {
+    console.log("Nenhum usuário está logado.");
   }
 });
