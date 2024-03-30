7d5b2cceff0f14b065fad2b3334165e08f0ba34b
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-03-30 18:49:05 -0300

    create: arquivo utils para separar pequenos codigos todoList-firebase

--- todoList/src/login/index.js
+++ todoList/src/login/index.js
@@ -1,6 +1,8 @@
 import { db } from '../config/firestore.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
 
+import { utils } from '../utils/utils.js';
+
 // let email = 'gokufirestore@email.com'
 // let password = '123456'
 
@@ -9,12 +11,6 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 
 const authSignIn = document.querySelector('[data-authentication="auth"]')
 
-const iconSelect = `
-	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-<title>checkbox-checked</title>
-<path fill="green" d="M28 0h-24c-2.2 0-4 1.8-4 4v24c0 2.2 1.8 4 4 4h24c2.2 0 4-1.8 4-4v-24c0-2.2-1.8-4-4-4zM14 24.828l-7.414-7.414 2.828-2.828 4.586 4.586 9.586-9.586 2.828 2.828-12.414 12.414z"></path>
-</svg>
-`
 // fazer login
 authSignIn.addEventListener('submit', (event) => {
 	event.preventDefault();
@@ -27,7 +23,7 @@ authSignIn.addEventListener('submit', (event) => {
 	  .then((userCredential) => { 
 	    const user = userCredential.user;
 	   	console.log('auth ', user.uid)
-	   	document.querySelector('.h2-login').innerHTML = `<i>${iconSelect}</i>`;
+	   	document.querySelector('.h2-login').innerHTML = `<i>${utils.iconSelect}</i>`;
 	   	
 	    setTimeout(() => {
 	      window.location = './todoList.html';
