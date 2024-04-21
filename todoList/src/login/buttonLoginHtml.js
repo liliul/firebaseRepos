fe2cd56ec1f0c56de3423a2b2e228689a38df740
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-04-21 19:34:26 -0300

    fix: button login no formulario todoList

--- todoList/src/login/buttonLoginHtml.js
+++ todoList/src/login/buttonLoginHtml.js
@@ -1,17 +1,5 @@
 const clickButtonLogin = document.getElementById('b-login');
 
 clickButtonLogin.addEventListener('click', () => {
-	document.querySelector('.form-control').innerHTML = `
-		<form class="form-login" data-authentication="auth">
-			<h2 class="h2-login">Fazer Login</h2>
-
-			<label class="label-login" for="Email">Email</label>
-			<input class="input-login" type="email" id="Email" name="Email" required placeholder="Digite seu email">
-
-			<label class="label-login" for="PassWord">Password</label>
-			<input class="input-login" type="password" id="PassWord" name="PassWord" required placeholder="Digite sua senha">
-
-			<button class="button-login" type="submit">Enviar</button>
-		</form>
-	`;
+	window.location.reload();
 })
