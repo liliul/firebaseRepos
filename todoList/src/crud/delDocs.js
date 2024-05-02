5d8d7035270c40ee62148ef43e173f8ced5cf082
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-05-02 18:40:30 -0300

    add: feature mensagem na delDocs todoList

--- todoList/src/crud/delDocs.js
+++ todoList/src/crud/delDocs.js
@@ -1,5 +1,6 @@
 import { db } from '../config/firestore.js';
 import { doc, deleteDoc } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js';
+import { mensagem } from '../utils/mensagem.js';
 
 const containerAddTask = document.querySelector('.main_content');
 
@@ -8,6 +9,8 @@ containerAddTask.addEventListener('click', async (e) => {
 
     if (eventDel) {
         await deleteDoc(doc(db, "todo-list", eventDel));
+
+        mensagem('Tarefa Deletada com Sucesso');
     }
 })
 
