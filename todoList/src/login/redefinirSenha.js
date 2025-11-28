a79c80254d9278f820421d1b728797519b863788
liliul <liliuestudo@gmail.com>
2025-11-27 21:40:07 -0300

    feat: RedefinirSenha todoList

--- todoList/src/login/redefinirSenha.js
+++ todoList/src/login/redefinirSenha.js
@@ -2,40 +2,43 @@ import { getAuth, sendPasswordResetEmail } from 'https://www.gstatic.com/firebas
 import { emailVerificadoMensagem } from '../utils/mensagem.js';
 import { utils } from '../utils/utils.js';
 
-const redefinirSenha = document.getElementById('redefinirSenha');
+export function RedefinirSenha() {
+	const redefinirSenha = document.getElementById('redefinirSenha');
 
-redefinirSenha.addEventListener('click', (e) => {
-	const auth = getAuth();
+	redefinirSenha.addEventListener('click', (e) => {
+		const auth = getAuth();
 
-	document.querySelector('.div-redefinir').style.display = 'none';
-	document.querySelector('#b-login').removeAttribute('disabled')
+		document.querySelector('.div-redefinir').style.display = 'none';
+		document.querySelector('#b-login').removeAttribute('disabled')
 
-	document.querySelector('.form-control').innerHTML = `
-		<form id="redefinir-senha" class="form-login">
-			<h2 class="h2-login">Redefinir Senha</h2>
+		document.querySelector('.form-control').innerHTML = `
+			<form id="redefinir-senha" class="form-login">
+				<h2 class="h2-login">Redefinir Senha</h2>
 
-			<label class="label-login" for="redefinir-email">Email</label>
-			<input class="input-login" type="email" id="redefinir-email" name="Email-criar" required placeholder="Digite seu email">
+				<label class="label-login" for="redefinir-email">Email</label>
+				<input class="input-login" type="email" id="redefinir-email" name="Email-criar" required placeholder="Digite seu email">
 
-			<button id="enviarEmail" class="button-login" type="submit">enviar senha</button>
-		</form>
-	`;
+				<button id="enviarEmail" class="button-login" type="submit">enviar senha</button>
+			</form>
+		`;
 
-	document.querySelector('#redefinir-senha').addEventListener('submit', async (e) => {
-		e.preventDefault();
+		document.querySelector('#redefinir-senha').addEventListener('submit', async (e) => {
+			e.preventDefault();
+			
+			const emailRedefinirSenha = document.getElementById('redefinir-email').value.trim();
+			
+			await sendPasswordResetEmail(auth, emailRedefinirSenha);
+
+			emailVerificadoMensagem('.isolate-login', `Email enviado para redefinir senha Email: ${emailRedefinirSenha}`);
+			
+			setTimeout(() => {
+				utils.renderizarTelaLogin()
+			}, 5000);
+		})
 		
-		const emailRedefinirSenha = document.getElementById('redefinir-email').value.trim();
-		
-		await sendPasswordResetEmail(auth, emailRedefinirSenha);
-
-		emailVerificadoMensagem('.isolate-login', `Email enviado para ${emailRedefinirSenha}`);
-		
-		setTimeout(() => {
-			utils.renderizarTelaLogin()
-		}, 5000);
 	})
-	
-})
+}
 
+RedefinirSenha()
 
 // auth/missing-email
